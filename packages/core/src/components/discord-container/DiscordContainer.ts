3144986e6b5ee074094de16bc1e530f852ae1fef
Nick <nick@ostra.gg>
2025-09-12 17:46:54 -0400

    feat(components-v2): add placeholder elements for components v2

---
+++ packages/core/src/components/discord-container/DiscordContainer.ts
@@ -0,0 +1,27 @@
+import { consume } from '@lit/context';
+import { css, html, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import type { LightTheme } from '../../types.js';
+import { messagesLightTheme } from '../discord-messages/DiscordMessages.js';
+
+@customElement('discord-container')
+export class DiscordContainer extends LitElement implements LightTheme {
+	/**
+	 * @internal
+	 */
+	public static override readonly styles = css``;
+
+	@consume({ context: messagesLightTheme, subscribe: true })
+	@property({ type: Boolean, reflect: true, attribute: 'light-theme' })
+	public accessor lightTheme = false;
+
+	protected override render() {
+		return html`<slot></slot>`;
+	}
+}
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'discord-container': DiscordContainer;
+	}
+}
---
+++ packages/core/src/components/discord-media-gallery/DiscordMediaGallery.ts
@@ -0,0 +1,27 @@
+import { consume } from '@lit/context';
+import { css, html, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import type { LightTheme } from '../../types.js';
+import { messagesLightTheme } from '../discord-messages/DiscordMessages.js';
+
+@customElement('discord-media-gallery')
+export class DiscordMediaGallery extends LitElement implements LightTheme {
+	/**
+	 * @internal
+	 */
+	public static override readonly styles = css``;
+
+	@consume({ context: messagesLightTheme, subscribe: true })
+	@property({ type: Boolean, reflect: true, attribute: 'light-theme' })
+	public accessor lightTheme = false;
+
+	protected override render() {
+		return html`<slot></slot>`;
+	}
+}
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'discord-media-gallery': DiscordMediaGallery;
+	}
+}
